d379202f247eb6b5557fe5fd9418edcecff3ab1c
Mila1213 <tu_correo@ejemplo.com>
2026-05-27 08:07:28 -0500

    fix firebase render

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,16 +1,20 @@
 const admin = require("firebase-admin");
 
-admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY
+        .replace(/\\n/g, "\n"),
+    }),
+  });
+}
 
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-  }),
-});
-
-const auth = admin.auth();
 const db = admin.firestore();
+const auth = admin.auth();
 
-module.exports = { auth, db };
+module.exports = {
+  db,
+  auth,
+};
