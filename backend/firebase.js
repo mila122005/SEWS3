52616fb8473253968e1d5b90990327f42ba23c08
Mila1213 <tu_correo@ejemplo.com>
2026-05-27 08:25:04 -0500

    firebase final fix

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,14 +1,18 @@
 const admin = require("firebase-admin");
 
+// Verificar variable
+if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
+  throw new Error("Falta FIREBASE_SERVICE_ACCOUNT");
+}
+
+// Convertir JSON string a objeto
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT
+);
+
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-    }),
+    credential: admin.credential.cert(serviceAccount),
   });
 }
 
